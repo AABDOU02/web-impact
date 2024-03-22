e7019b11f684e32365b8ae247c22c6d1dddccaef
Abdelkhalek <64686425+AABDOU02@users.noreply.github.com>
2024-03-22 10:33:58 +0100

    Update Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,7 +5,7 @@ import './Navbar.css'; // Import your CSS file for custom styles
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <Link className="navbar-brand" to="/">Web Impact</Link>
+      <Link className="navbar-brand" to="/web-impact">Web Impact</Link>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
